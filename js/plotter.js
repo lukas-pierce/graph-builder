f9715527ab6060418e307fcc9730fbd9275fe1df
lukas <kosha.industry@gmail.com>
2019-10-09 10:47:39 +0600

    add plotter.js

---
+++ js/plotter.js
@@ -0,0 +1,48 @@
+export class Plotter {
+
+  scale = 10;
+  x0 = 0;
+  y0 = 0;
+
+  points = [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ];
+
+  _coordinates(point) {
+    const x = point[0] * this.scale + this.x0;
+    const y = point[1] * this.scale * -1 + this.y0;
+    return [x, y];
+  }
+
+  render(canvas) {
+    const ctx = canvas.getContext('2d');
+
+    // init center
+    this.x0 = ctx.canvas.clientWidth / 2;
+    this.y0 = ctx.canvas.clientHeight / 2;
+
+    // render Y axis
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#00f';
+    ctx.moveTo(this.x0, 0);
+    ctx.lineTo(this.x0, ctx.canvas.clientHeight);
+    ctx.stroke();
+
+    // render X axis
+    ctx.moveTo(0, this.y0);
+    ctx.lineTo(ctx.canvas.clientWidth, this.y0);
+    ctx.stroke();
+
+    ctx.strokeStyle = '#000';
+    ctx.beginPath();
+    const startPoint = this.points[0];
+    ctx.moveTo(...this._coordinates(startPoint));
+    this.points.splice(1).forEach(point => {
+      ctx.lineTo(...this._coordinates(point))
+    });
+    ctx.stroke();
+  }
+
+}
